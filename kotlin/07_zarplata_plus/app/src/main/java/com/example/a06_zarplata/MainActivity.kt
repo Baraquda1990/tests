b6ce40ca2278a42ae747e6e70978067b8e039ac1
DamirWork <damir.gilmanov2015@yandex.ru>
2024-05-13 10:03:28 +0500

    130524

--- kotlin/07_zarplata_plus/app/src/main/java/com/example/a06_zarplata/MainActivity.kt
+++ kotlin/07_zarplata_plus/app/src/main/java/com/example/a06_zarplata/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.a06_zarplata
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.example.a06_zarplata.databinding.ActivityMainBinding
 
@@ -14,23 +15,56 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         bindingClass.but.setOnClickListener {
+            bindingClass.img.visibility=View.VISIBLE
             var name:String=bindingClass.textBox.text.toString()
             var label=bindingClass.label
             var pass=bindingClass.pass.text.toString()
             when(name){
                 Constance.DVORNIC->{
-                    if(pass==Constance.DVORNIC_PASS) label.text="Твоя зп ${Constance.DVORNIC_ZP}тг. Нищеброд!"
-                        else label.text="Не тот порол"}
+                    if(pass==Constance.DVORNIC_PASS) {
+                        bindingClass.img.setImageResource(R.drawable.face_co_1)
+                        label.text="Твоя зп ${Constance.DVORNIC_ZP}тг. Нищеброд!"
+                    }
+                    else {
+                        bindingClass.img.setImageResource(R.drawable.dula)
+                        label.text="Не тот порол"
+                    }
+                }
                 Constance.ZAMDIR->{
-                    if(pass==Constance.ZAMDIR_PASS) label.text="Получи свои ${Constance.ZAMDIR_ZP}тг. ЗамДир ебаный!!"
-                    else label.text="Не тот порол"}
+                    if(pass==Constance.ZAMDIR_PASS) {
+                        bindingClass.img.setImageResource(R.drawable.face_co_2)
+                        label.text="Получи свои ${Constance.ZAMDIR_ZP}тг. ЗамДир ебаный!!"
+                    }
+                    else {
+                        bindingClass.img.setImageResource(R.drawable.dula)
+                        label.text="Не тот порол"
+                    }
+                }
                 Constance.AKIM->{
-                    if(pass==Constance.AKIM_PASS) label.text="Твоя зп. ${Constance.AKIM_ZP}тг. Аким ворующий у народа. Ирод, сука, тварь, паскуда!!!"
-                    else label.text="Не тот порол"}
+                    if(pass==Constance.AKIM_PASS) {
+                        bindingClass.img.setImageResource(R.drawable.face_co_2)
+                        label.text="Твоя зп. ${Constance.AKIM_ZP}тг. Аким ворующий у народа. Ирод, сука, тварь, паскуда!!!"
+                    }
+                    else {
+                        bindingClass.img.setImageResource(R.drawable.dula)
+                        label.text="Не тот порол"
+                    }
+                }
                 Constance.PRESIDENT->{
-                    if(pass==Constance.PRESIDENT_PASS) label.text="Здравтсвуйте ГОСПОДИН Владимирович!"
-                    else label.text="Не тот порол"}
-                else->label.text="Я тебя не знаю."
+                    if(pass==Constance.PRESIDENT_PASS) {
+                        bindingClass.img.setImageResource(R.drawable.face_co_2)
+                        label.text="Здравтсвуйте ГОСПОДИН Владимирович!"
+                    }
+                    else {
+                        bindingClass.img.setImageResource(R.drawable.dula)
+                        label.text="Не тот порол"
+                    }
+                }
+
+                else->{
+                    bindingClass.img.setImageResource(R.drawable.dula)
+                    label.text="Я тебя не знаю."
+                }
             }
         }
     }
